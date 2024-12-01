61421951d2941014a2b485a634eb4d8caf3f0201
GSC <gsc.code@gmail.com>
2024-12-01 19:28:08 +0100

    Add project files.

---
+++ DataGridExporter/Classes/MudDataGridExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using MudBlazor;
+
+public static class MudDataGridExtensions
+{
+	public static async Task DownloadExcel<T>(this MudDataGrid<T> grid, IJSRuntime js, string filename)
+	{
+		ExcelWriter excelWriter = new ExcelWriter();
+		byte[] content = excelWriter.GenerateExcel(grid.RenderedColumns, grid.FilteredItems);
+
+		await JSRuntimeExtensions.InvokeAsync<object>(js, "saveAsFile", new object[2]
+		{
+					filename,
+					Convert.ToBase64String(content)
+		});
+	}
+}
