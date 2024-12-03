59dcee8789bb2d2351a7b1de3a8e73a90084cda1
GSC <gsc.code@gmail.com>
2024-12-03 13:20:48 +0100

    Saving changes.

---
+++ DataGridExporter/Classes/Cell.cs
@@ -0,0 +1,8 @@
+﻿public class Cell(string content)
+{
+	public string Content { get; set; } = content;
+	public int ColumnIndex { get; set; }
+	public int RowIndex { get; set; }
+	public int ColSpan { get; set; } = 1;
+	public int RowSpan { get; set; } = 1;
+}
---
+++ DataGridExporter/Classes/CellData.cs
@@ -0,0 +1,7 @@
+﻿public class CellData(bool extendedSheet)
+{
+	public CellData() : this(false) { }
+	public List<List<Cell>> Cells { get; set; } = new List<List<Cell>>();
+	public string? SheetName { get; set; }
+	public bool ExtendedSheet { get; set; } = extendedSheet;
+}
