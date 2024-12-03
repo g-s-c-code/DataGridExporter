2fd06726db68220716d322efa79c3b5cc232daa0
GSC <gsc.code@gmail.com>
2024-12-03 09:53:35 +0100

    Saving progress.

---
+++ DataGridExporter/wwwroot/js/copyCode.js
@@ -0,0 +1,12 @@
+﻿function copyCode() {
+    const codeElement = document.getElementById('code');
+    if (codeElement) {
+        navigator.clipboard.writeText(codeElement.innerText)
+            .then(() => {
+                alert('Code copied to clipboard!');
+            })
+            .catch(err => {
+                console.error('Failed to copy text: ', err);
+            });
+    }
+}
