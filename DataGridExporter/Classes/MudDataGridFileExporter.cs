f8099f3874a8d501820c2565cfa4559bdc381009
GSC <gsc.code@gmail.com>
2024-12-03 13:41:52 +0100

    Saving progress.

--- DataGridExporter/Classes/MudDataGridFileExporter.cs
+++ DataGridExporter/Classes/MudDataGridFileExporter.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.JSInterop;
 using MudBlazor;
 
-public static class MudDataGridExtensions
+public static class MudDataGridFileExporter
 {
-	public static async Task ExportDataGrid<T>(this MudDataGrid<T> grid, IJSRuntime js, string filename)
+	public static async Task ExportMudDataGrid<T>(this MudDataGrid<T> grid, IJSRuntime js, string filename)
 	{
 		CellWriter spreadsheetlWriter = new CellWriter();
 		byte[] content = spreadsheetlWriter.GenerateSpreadsheet(grid.RenderedColumns, grid.FilteredItems);
 
 		await JSRuntimeExtensions.InvokeAsync<object>(js, "saveAsFile", new object[2]
 		{
-					filename,
-					Convert.ToBase64String(content)
+					filename, Convert.ToBase64String(content)
 		});
 	}
 }
--- DataGridExporter/wwwroot/js/saveFileAs.js
+++ DataGridExporter/wwwroot/js/saveFileAs.js
@@ -1,8 +1,38 @@
-﻿function saveAsFile(filename, bytesBase64) {
-    var link = document.createElement('a');
-    link.download = filename;
-    link.href = "data:application/octet-stream;base64," + bytesBase64;
-    document.body.appendChild(link); // Needed for Firefox
-    link.click();
-    document.body.removeChild(link);
+﻿/**
+ * Save file from base64 encoded data
+ * @param {string} filename - Name of the file to be saved
+ * @param {string} bytesBase64 - Base64 encoded file content
+ * @param {string} [mimeType='application/octet-stream'] - MIME type of the file
+ * @returns {boolean} - Indicates whether the file save was initiated successfully
+ */
+function saveAsFile(filename, bytesBase64, mimeType = 'application/octet-stream') {
+    try {
+        if (!filename || !bytesBase64) {
+            console.error('Filename and base64 data are required');
+            return false;
+        }
+
+        const byteCharacters = atob(bytesBase64);
+        const byteNumbers = new Array(byteCharacters.length);
+        for (let i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        const blob = new Blob([byteArray], { type: mimeType });
+
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = filename;
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        URL.revokeObjectURL(link.href);
+
+        return true;
+    } catch (error) {
+        console.error('Error saving file:', error);
+        return false;
+    }
 }
